f6649ac052b69134ea18b4cf783195dbf8e99903
dhatrisriram <dhatrisriram.1107@gmail.com>
2025-04-10 10:11:07 +0530

    Update test_routing.py

--- test_routing.py
+++ test_routing.py
@@ -1,8 +1,7 @@
 import requests
 import json
 
-# Replace with your HERE Maps API Key
-API_KEY = "aMdlpzV__Fi_dsa66B8zZEa1jmfg21jBTXsTy-Yjl0Q"
+API_KEY = HERE Maps API Key
 
 # Define the API URL
 url = f"https://router.hereapi.com/v8/routes?transportMode=car&origin=52.5309837,13.3845671&destination=52.5263797,13.3686127&return=summary&apikey={API_KEY}"
